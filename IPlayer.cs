9f5b1885413d10a0a0242cedcdf498ed7be2f5ec
AleksandarT5 <todorov_alex@abv.bg>
2020-01-09 15:12:38 +0200

    Add files via upload

---
+++ IPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Santase
+{
+    interface IPlayer
+    {
+        string Name { get; }
+
+        List<Card> CardsPlayer { get; }
+
+        int Points { get; }
+
+        int Games { get; }
+
+        bool IsFirstPlay { get; }
+    }
+}
---
+++ StartUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Santase
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            // Пълнене на тестето и разбъркване на картите
+
+                //DeckOfCards    
+
+            Player opponent = new Player(Console.ReadLine(), new List<Card>(), 0, 0, true);
+            Player player = new Player(Console.ReadLine(), new List<Card>(), 0, 0, false);
+            // Раздаване
+
+                //Board
+
+            // Игра
+
+        }
+    }
+}
