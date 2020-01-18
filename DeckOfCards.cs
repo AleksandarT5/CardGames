82ff85985100537444892ce3ebbf1e5265e59789
AleksandarT5 <todorov_alex@abv.bg>
2020-01-19 00:34:28 +0200

    Add files via upload

--- DeckOfCards.cs
+++ DeckOfCards.cs
@@ -1,11 +1,131 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Santase
 {
-    internal class DeckOfCards
+    internal class DeckOfCards : IDeckOfCards
     {
+        private string[] valuesCards = { "9", "J", "D", "K", "10", "A" };
 
+        public List<Card> GameCards { get; set; } = new List<Card>();
+        public Card OpenTrumpCard { get; set; }
+
+        public DeckOfCards()
+        {
+            CreateDeck();
+        }
+
+        private void CreateDeck()
+        {
+            FillDeckOfCards(this.GameCards, this.valuesCards);
+            FillDeck(this.GameCards);
+        }
+
+
+        public List<Card> FillDeckOfCards(List<Card> cards, string[] valuesCards)
+        {
+            for (int i = 0; i < valuesCards.Length; i++)
+            {
+                cards.Add(new Card(TypeCard.Club.ToString(), valuesCards[i], 0));
+                cards.Add(new Card(TypeCard.Diamond.ToString(), valuesCards[i], 0));
+                cards.Add(new Card(TypeCard.Heart.ToString(), valuesCards[i], 0));
+                cards.Add(new Card(TypeCard.Spade.ToString(), valuesCards[i], 0));
+            }
+
+            return cards;
+        }
+
+        public void FillDeck(List<Card> deckOfCards)
+        {
+            Random random = new Random();
+            for (int i = 0; i < 24; i++)
+            {
+                int cardNumber = random.Next(0, deckOfCards.Count);
+                var newPosition = deckOfCards[cardNumber];
+                deckOfCards[cardNumber] = deckOfCards[i];
+                deckOfCards[i] = newPosition;
+            }
+        }
+
+        public void OneHandingOutCards(Player participant, Player secondParticipant, int count = 1)
+        {
+            var current = count % 2 == 0 ? secondParticipant : participant;
+            HandingOutCardHand(this.GameCards, current);
+            if (count < 4)
+            {
+                count += 1;
+                OneHandingOutCards(participant, secondParticipant, count);
+            }
+            participant.CardsPlayer = participant.CardsPlayer.OrderBy(t => t.Type).ThenByDescending(v => v.Value).ToList();
+            //Console.WriteLine(string.Format($"Opponent cards: {string.Join(", ", participant.CardsPlayer)}"));
+
+            secondParticipant.CardsPlayer = secondParticipant.CardsPlayer.OrderBy(t => t.Type).ThenByDescending(v => v.Value).ToList();
+            //Console.WriteLine(string.Format($"Player cards: {string.Join(", ", secondParticipant.CardsPlayer)}"));
+        }
+
+        private void HandingOutCardHand(List<Card> basicDeckOfCards, Player participant)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Card oneCard = basicDeckOfCards[basicDeckOfCards.Count - 1];
+                participant.CardsPlayer.Add(oneCard);
+                basicDeckOfCards.Remove(oneCard);
+            }
+        }
+
+        public Card GetTrumpCard()
+        {
+            var card = this.GameCards[this.GameCards.Count - 1];
+            this.GameCards.RemoveAt(this.GameCards.Count - 1);
+            return card;
+        }
+
+        public void TakeCards(Player opponent, Player player, Card openTrumpCard)
+        {
+            // проверка за == 1
+            if (this.GameCards.Count > 1)
+            {
+                if (opponent.IsFirstPlay == true)
+                {
+                    TakeCardsFromTheBase(opponent, player, this.GameCards);
+                }
+
+                else
+                {
+                    TakeCardsFromTheBase(player, opponent, this.GameCards);
+                }
+            }
+
+            else
+            {
+                if (opponent.IsFirstPlay == true)
+                {
+                    TakeLastCardFromTheBase(opponent, player, this.GameCards, openTrumpCard);
+                }
+
+                else
+                {
+                    TakeLastCardFromTheBase(player, opponent, this.GameCards, openTrumpCard);
+                }
+            }
+
+        }
+
+        private void TakeCardsFromTheBase(Player winner, Player lost, List<Card> basicCards)
+        {
+            winner.CardsPlayer.Add(basicCards[basicCards.Count - 1]);
+            basicCards.Remove(basicCards[basicCards.Count - 1]);
+            lost.CardsPlayer.Add(basicCards[basicCards.Count - 1]);
+            basicCards.Remove(basicCards[basicCards.Count - 1]);
+        }
+
+        private void TakeLastCardFromTheBase(Player winner, Player lost, List<Card> basicCards, Card openTrumpCard)
+        {
+            winner.CardsPlayer.Add(basicCards[0]);
+            winner.CardsPlayer.Remove(basicCards[0]);
+            lost.CardsPlayer.Add(openTrumpCard);
+        }
     }
 }
--- StartUp.cs
+++ StartUp.cs
@@ -8,17 +8,20 @@ namespace Santase
         static void Main(string[] args)
         {
             // Пълнене на тестето и разбъркване на картите
-
-                //DeckOfCards    
+            DeckOfCards deckOfCards = new DeckOfCards();
 
             Player opponent = new Player(Console.ReadLine(), new List<Card>(), 0, 0, true);
             Player player = new Player(Console.ReadLine(), new List<Card>(), 0, 0, false);
-            // Раздаване
+
+            while (true)
+            {
+                // Раздаване
 
                 //Board
 
-            // Игра
+                // Игра
 
+            }           
         }
     }
 }
